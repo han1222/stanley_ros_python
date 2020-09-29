681babd5e9a442709904aa313708fb927fac0298
SEUNGIL HAN <35947629+han1222@users.noreply.github.com>
2020-09-30 00:14:47 +0900

    add feedforward term and debugging session

--- stanley_tram/src/stanley_tram.py
+++ stanley_tram/src/stanley_tram.py
@@ -7,34 +7,59 @@ from std_msgs.msg import Float32MultiArray
 from std_msgs.msg import Float32 
 from nav_msgs.msg import Odometry
 
-max_rad=0.523598 #I added margin
-min_rad=-0.523598
-soft_term=0.01
+wheel_base=2.645
+steering_ratio = 22.462
+steering_vel_constraint = 80
+max_rad=0.55 #I added margin maixmum is 0.6526438369 [rad]
+min_rad=-0.55
+soft_term=0.2
 global velocity
 velocity=0
-# Please change gain here for your system
-gain=0.5
 
+p_gain=0.06#0.15#0.06
+h_gain =0.1#0.15
+f_gain=0.3
+dt=0.025#0.025
+global prev_delta
+prev_delta=0
 rospy.init_node('stanley')
 
 def error(msg):
+    
     global velocity
+    global prev_delta
     x = symbols('x')
     fx = msg.data[0] * x ** 3 + msg.data[1] * x ** 2 +  msg.data[2] * x ** 1 + msg.data[3]
     fprime = Derivative(fx, x).doit()
-    n = fprime.subs({x: 2.6})
-    cte=fx.subs({x: 2.6}) # check this today
-    crosstrack_error_term=math.atan((gain*cte)/(velocity+soft_term))
-    heading_error_term=math.atan(n)
-    #print("heading_error term: ",heading_error_term )
-    #print("crosstrack_error_term: ",crosstrack_error_term)
-    delta= crosstrack_error_term+ heading_error_term
+    f_2_prime=Derivative(fprime, x).doit()
+    n = fprime.subs({x:9.6})
+    n_2=f_2_prime.subs({x: 2.6})
+    radius=((pow(n,2)+1)*(math.sqrt(pow(n,2)+1))) / n_2
+    feedforward_term=math.atan(wheel_base/radius)
+    cte=1*fx.subs({x: 9.6}) # check this today
+    crosstrack_error_term=math.atan((p_gain*cte)/(velocity+soft_term))
+    heading_error_term=1*math.atan(n)
+    #delta= crosstrack_error_term+ h_gain*heading_error_term
+    delta= crosstrack_error_term+ h_gain * heading_error_term+f_gain*feedforward_term
     if max_rad < delta:
         delta=max_rad
     elif delta< min_rad: 
         delta=min_rad
 
+    if ((((delta-prev_delta)* steering_ratio * 180) / math.pi)/dt > steering_vel_constraint) :
+        delta=prev_delta + steering_vel_constraint * dt * math.pi / 180 / steering_ratio
+    elif  ((((delta-prev_delta)* steering_ratio * 180) / math.pi)/dt < -steering_vel_constraint):
+        delta=prev_delta - steering_vel_constraint * dt * math.pi / 180 / steering_ratio
+
+    
+    print("------------------------------------")
+    print("Steering_angle: ",delta * steering_ratio * 180 / math.pi)
+    print("Cross Track Eror: ", cte, "Position Command: ", crosstrack_error_term  * steering_ratio * 180 / math.pi)
+    print("Heading Error Term: ", h_gain*heading_error_term *  steering_ratio * 180 / math.pi)
+    #print("angular_velocity:",(((delta-prev_delta) / math.pi)/dt))
+    prev_delta=delta
     pub.publish(delta)
+
 
 def get_odom_cb(msg):
     global velocity
